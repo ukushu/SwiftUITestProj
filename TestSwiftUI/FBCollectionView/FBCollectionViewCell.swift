18f2e6b5d1187319059416c737eb8461540731ee
UKS <skulptorrr@gmail.com>
2023-03-31 04:04:54 +0300

    garbage removed

--- TestSwiftUI/FBCollectionView/FBCollectionViewCell.swift
+++ TestSwiftUI/FBCollectionView/FBCollectionViewCell.swift
@@ -11,33 +11,3 @@ final class CollectionViewItem : NSCollectionViewItem {
         self.view = container
     }
 }
-
-//final class FBCollectionViewCell<Content: View>: NSCollectionViewItem {
-//    var selectedCGColor: CGColor    { Color(rgbaHex: 0x00900050).cgColor! }
-//    var nonSelectedCGColor: CGColor { NSColor.clear.cgColor }
-//
-//    // TODO: also highlight/hover state!
-//    // TODO: pass to Content
-//    override var isSelected: Bool {
-//        didSet {
-//            if isSelected {
-//                view.layer?.backgroundColor = selectedCGColor
-//                view.layer?.masksToBounds = true
-//                view.layer?.cornerRadius = 5.0
-//            } else {
-//                view.layer?.backgroundColor = nonSelectedCGColor
-//                view.layer?.masksToBounds = false
-//            }
-//        }
-//    }
-//
-//    var contents: NSView?
-//    let container = NSStackView()
-//
-//    override func loadView() {
-//        container.orientation = NSUserInterfaceLayoutOrientation.vertical
-//        container.wantsLayer = true
-//
-//        self.view = container
-//    }
-//}
