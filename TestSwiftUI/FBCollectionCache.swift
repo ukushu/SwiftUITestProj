c320431c8c8b468c6505b44137b89ce4494b79a2
UKS <skulptorrr@gmail.com>
2023-04-05 18:08:57 +0300

    FBCollectionCache WIP

--- TestSwiftUI/FBCollectionCache.swift
+++ TestSwiftUI/FBCollectionCache.swift
@@ -2,27 +2,55 @@ import Foundation
 import SwiftUI
 import QuickLook
 import QuickLookThumbnailing
+import Darwin
 
 public class FBCollectionCache {
+    private static var cache: [String : FBCCacheItem] = [:]
     
+    static func getCachedImg(path: String) -> FBCCacheItem? {
+        if let img = cache[path] {
+            return img
+        }
+        
+        cache[path] = FBCCacheItem(path: path)
+        
+        
+        
+//        let cahceSizeBytes = class_getInstanceSize(FBCCacheItem.self) * cache.count
+//        print("Cache size: \(cahceSizeBytes)")
+        
+        
+        return cache[path]
+    }
     
-    
-    
+    static func clearCache() {
+        cache = [:]
+    }
 }
 
 class FBCCacheItem {
-    private(set) var icon: NSImage? = nil
     private(set) var thumbnail: NSImage? = nil
-    let date: Date
+    private(set) var date: Date = Date.now
+    private let path: String
     
     init(path: String) {
+        self.path = path
+        updateThumbnail()
+    }
+    
+    func updateThumbnail() {
         self.date = Date.now
         
         DispatchQueue.global(qos: .background).async {
-            self.thumbnail = imgThumbnailAdv(125, path: path)
+            self.thumbnail = imgThumbnailAdv(125, path: self.path)
         }
     }
 }
+
+
+//////////////////////////////////
+///HELPERS
+/////////////////////////////////
 
 fileprivate func imgThumbnailAdv(_ size: CGFloat, path: String) -> NSImage? {
     if path.FS.info.isDirectory {
@@ -50,3 +78,51 @@ fileprivate func imgThumbnailAdv(_ size: CGFloat, path: String) -> NSImage? {
     
     return path.FS.info.hiresThumbnail(size: size)
 }
+
+extension NSImage{
+    var pixelSize: NSSize?{
+        if let rep = self.representations.first{
+            let size = NSSize(width: rep.pixelsWide, height: rep.pixelsHigh)
+            return size
+        }
+        return nil
+    }
+}
+
+
+class IconCache {
+    private static let musicIcon = NSImage(named: "MusicIcon")
+    
+    static func getIcon(path: String) -> NSImage? {
+        if let mimeType = path.FS.info.mimeType, mimeType.conforms(to: .audio) {
+            return musicIcon
+        }
+        
+        return NSWorkspace.shared.icon(forFile: path)
+    }
+}
+
+
+//func allocatedMem() {
+//    let TASK_VM_INFO_COUNT = MemoryLayout<task_vm_info_data_t>.size / MemoryLayout<natural_t>.size
+//
+//    var vmInfo = task_vm_info_data_t()
+//    var vmInfoSize = mach_msg_type_number_t(TASK_VM_INFO_COUNT)
+//
+//    let kern: kern_return_t = withUnsafeMutablePointer(to: &vmInfo) {
+//            $0.withMemoryRebound(to: integer_t.self, capacity: 1) {
+//                task_info(mach_task_self_,
+//                          task_flavor_t(TASK_VM_INFO),
+//                          $0,
+//                          &vmInfoSize)
+//                }
+//            }
+//
+//    if kern == KERN_SUCCESS {
+//        let usedSize = DataSize(bytes: Int(vmInfo.internal + vmInfo.compressed))
+//        print("Memory in use (in bytes): %u", usedSize)
+//    } else {
+//        let errorString = String(cString: mach_error_string(kern), encoding: .ascii) ?? "unknown error"
+//        print("Error with task_info(): %s", errorString);
+//    }
+//}
--- TestSwiftUI/Helpers/SwiftUI/UksImagePath.swift
+++ TestSwiftUI/Helpers/SwiftUI/UksImagePath.swift
@@ -1,128 +1,47 @@
-///////////////////////////
-// OPTIMIZED
-//////////////////////////
-
-
-
-
-
 import Foundation
 import SwiftUI
 import Essentials
 import AsyncNinja
 
 struct UKSImagePath: View {
-    let path: String
-    let size: CGFloat
+    @ObservedObject var model: UKSImagePathVM
     
-    @State private var thumbnail: NSImage? = nil //{ didSet{ print("path ZZ: \(path)") } }
+    init(path: String, size: CGFloat) {
+        model = UKSImagePathVM(path: path)
+    }
     
     var body: some View {
-        if let thumbnail = thumbnail {
+        if let thumbnail = model.thumbnail {
             Image(nsImage: thumbnail)
                 .resizable()
                 .scaledToFit()
         } else {
-            if let icon = IconCache.getIcon(path:path) {
+            if let icon = IconCache.getIcon(path: model.path) {
                 Image(nsImage: icon )
                     .resizable()
                     .scaledToFit()
-                    .onAppear(perform: generateThumbnail) // << here !!
             }
         }
     }
-    
-    func generateThumbnail() {
-        updThumbnailFromHdd()
-    }
-    
-    func updThumbnailFromHdd() {
-        DispatchQueue.global(qos: .background).async {
-            self.thumbnail = imgThumbnailAdv(size, path: path)
-        }
-    }
 }
 
-
-
-//struct UKSImage2: View {
-//    let url: URL
-//    let size: CGFloat
-//
-//    @State private var thumbnail: NSImage? = nil
-//
-//    var body: some View {
-//        if let thumbnail = thumbnail {
-//            Image(nsImage: thumbnail)
-//                .resizable()
-//                .scaledToFit()
-//        } else {
-//            Space(size)
-//              .onAppear(perform: generateThumbnail) // << here !!
-//        }
-//    }
-//
-//    func generateThumbnail() {
-//        DispatchQueue.global(qos: .background).async {
-//            self.thumbnail = url.imgThumbnailAdv(size)
-//        }
-//    }
-//}
-
-
-fileprivate extension URL {
-    func imgThumbnailAdv(_ size: CGFloat) -> NSImage? {
-        TestSwiftUI.imgThumbnailAdv(size, path: self.path)
-    }
-}
-
-
-fileprivate func imgThumbnailAdv(_ size: CGFloat, path: String) -> NSImage? {
-    if path.FS.info.isDirectory {
-        return path.FS.info.hiresQLThumbnail(size: size).wait().maybeSuccess ??
-               path.FS.info.hiresIcon(size: Int(size))
-    }
+class UKSImagePathVM: ObservableObject {
+    let path: String
+    @Published var thumbnail: NSImage?
     
-    let extensionsExceptions: [String]  = ["txt","docx","doc","pages","odt","rtf","tex","wpd","ltxd",
-                                           "btxt","dotx","wtt","dsc","me","ans","log","xy","text","docm",
-                                           "wps","rst","readme","asc","strings","docz","docxml","sdoc",
-                                           "plain","notes","latex","utxt","ascii",
-
-                                           "xlsx","patch","xls","xlsm","ods",
-
-                                           "py","cs","swift","html","css", "fountain","gscript","lua",
-
-                                           "markdown","md",
-                                           "plist", "ips"
-    ]
+    private(set) var timer: TimerCall!
     
-    if path.lowercased().hasSuffix(extensionsExceptions) {
-        return path.FS.info.hiresQLThumbnail(size: size).wait().maybeSuccess ??
-                path.FS.info.hiresIcon(size: Int(size))
-    }
-    
-    return path.FS.info.hiresThumbnail(size: size)
-}
-
-extension NSImage{
-    var pixelSize: NSSize?{
-        if let rep = self.representations.first{
-            let size = NSSize(width: rep.pixelsWide, height: rep.pixelsHigh)
-            return size
-        }
-        return nil
-    }
-}
-
-
-class IconCache {
-    private static let musicIcon = NSImage(named: "MusicIcon")
-    
-    static func getIcon(path: String) -> NSImage? {
-        if let mimeType = path.FS.info.mimeType, mimeType.conforms(to: .audio) {
-            return musicIcon
-        }
+    init(path: String) {
+        self.thumbnail = FBCollectionCache.getCachedImg(path: path)?.thumbnail
+        self.path = path
         
-        return NSWorkspace.shared.icon(forFile: path)
+        
+        self.timer = TimerCall(.continious(interval: 0.1)) {
+            let thumb = FBCollectionCache.getCachedImg(path: path)?.thumbnail
+            
+            if self.thumbnail != thumb {
+                self.thumbnail = thumb
+            }
+        }
     }
 }
