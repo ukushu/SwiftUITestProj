91221da28a301241b3fdf52f0ea52846d9da10f5
UKS <skulptorrr@gmail.com>
2023-04-14 12:02:19 +0300

    UksImagePath upd

--- TestSwiftUI/Helpers/SwiftUI/UksImagePath.swift
+++ TestSwiftUI/Helpers/SwiftUI/UksImagePath.swift
@@ -33,7 +33,7 @@ class UKSImagePathVM: ObservableObject {
     let path: String
     @Published var thumbnail: NSImage?
     
-    private(set) var timer: TimerCall!
+    private(set) var timer: TimerCall?
     
     var readyToLoad = false
     
@@ -41,15 +41,19 @@ class UKSImagePathVM: ObservableObject {
         self.thumbnail = FBCollectionCache.getCachedImg(path: path)?.thumbnail
         self.path = path
         
-        self.timer = TimerCall(.continious(interval: 0.09)) { [weak self] in
-            guard let self else { return }
-            
-            if self.readyToLoad {
-                let thumb = FBCollectionCache.getCachedImg(path: path)?.thumbnail
+        if thumbnail != nil {
+            timer = nil
+        } else {
+            self.timer = TimerCall(.continious(interval: 0.09)) { [weak self] in
+                guard let self else { return }
                 
-                //update only in case not the same
-                self.thumbnail = thumb
-                self.readyToLoad = false
+                if self.readyToLoad {
+                    let thumb = FBCollectionCache.getCachedImg(path: path)?.thumbnail
+                    
+                    //update only in case not the same
+                    self.thumbnail = thumb
+                    self.readyToLoad = false
+                }
             }
         }
     }
