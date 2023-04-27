be2104a57801754fdcfb959e598439b478d93d45
UKS <skulptorrr@gmail.com>
2023-04-27 05:30:33 +0300

    refactoring

--- TestSwiftUI/FBCollectionView/FBCollectionViewConroller.swift
+++ TestSwiftUI/FBCollectionView/FBCollectionViewConroller.swift
@@ -8,20 +8,18 @@ import Quartz
 public class NSCollectionController<T: RandomAccessCollection, Content: View>:
     NSViewController, NSCollectionViewDelegate, NSCollectionViewDataSource
 //QuickLook
-, QLPreviewPanelDataSource
+, QLPreviewPanelDataSource, QLPreviewPanelDelegate
 where T.Index == Int {
     
     let factory: (T.Element, IndexPath) -> Content
     
     let         id : String
     var         items : T
-    weak var    itemsView: NSCollectionView?
+    weak var    collectionView: NSCollectionView?
     var         selection : IndexSet {
         get { CollectionState.shared.selection }
         set { CollectionState.shared.selection = newValue }
     }
-    
-    //    public let parent: NSCollectionView
     
     let scrollToTopCancellable: AnyCancellable?
     
@@ -31,7 +29,7 @@ where T.Index == Int {
         self.id = id
         self.items = collection
         self.factory = factory
-        self.itemsView = collectionView
+        self.collectionView = collectionView
         self.scrollToTopCancellable = scrollToTopCancellable
         
         super.init(nibName: nil, bundle: nil)
@@ -43,77 +41,38 @@ where T.Index == Int {
         return makeItemForCV(byIndexPath: indexPath, collectionView: collectionView)
     }
     
-    func makeItemForCV(byIndexPath indexPath: IndexPath, collectionView: NSCollectionView) -> NSCollectionViewItem {
-        let itemIsEmptyCell = itemIsNil(indexPath)
-        
-        let item = collectionView.makeItem(withIdentifier: NSUserInterfaceItemIdentifier("NSCollectionViewItem"), for: indexPath)
-        
-        if let item = item as? CollectionViewItem {
-            let hosting = NSHostingView(rootView: factory(items[indexPath.item], indexPath))
-            
-            item.container.views.forEach { item.container.removeView($0) }
-            item.container.addView(hosting, in: .center)
-            
-            if itemIsEmptyCell {
-                item.acceptsMouseDown = false
-            }
-        }
-        
-        return item
-    }
-   
     
-    //    public func reloadData(at indexPath: Set<IndexPath>? ) {
-    //        if let indexPath = indexPath {
-    //            collectionView?.reloadItems(at: indexPath)
-    //        }
-    //
-    //        if let selection = selection {
-    //            if selection.wrappedValue.isEmpty && collection.count > 0 {
-    //                selection.wrappedValue = [0]
-    //            }
-    //        }
-    //    }
-    
-    
-    
-///////////////////////////////
-// HELPERS Selection update
-///////////////////////////////
+    ///////////////////////////////
+    // HELPERS Selection update
+    ///////////////////////////////
     public func collectionView(_ collectionView: NSCollectionView, didSelectItemsAt indexPaths: Set<IndexPath>) {
-        selLog("________________", indexPaths, collectionView)
+        selectionLog("________________", indexPaths, collectionView)
         self.selection = collectionView.selectionIndexes
-        selLog("didSelectItemsAt", indexPaths, collectionView)
+        selectionLog("didSelectItemsAt", indexPaths, collectionView)
     }
     
     public func collectionView(_ collectionView: NSCollectionView, didDeselectItemsAt indexPaths: Set<IndexPath>) {
-        selLog("_______________", indexPaths, collectionView)
+        selectionLog("_______________", indexPaths, collectionView)
         self.selection = collectionView.selectionIndexes
-        selLog("DESELECTItemsAt", indexPaths, collectionView)
+        selectionLog("DESELECTItemsAt", indexPaths, collectionView)
     }
     
     public func collectionView(_ collectionView: NSCollectionView, shouldSelectItemsAt indexPaths: Set<IndexPath>) -> Set<IndexPath> {
         return exceptNilItems(indexPaths)
     }
     
-    func selLog(_ title: String, _ indexPaths: Set<IndexPath>, _ collectionView: NSCollectionView) {
-//        print("""
-//              \(title):\t changes: \(indexPaths.map{ $0.intValue })\t|\tselInternal: \(collectionView.selectionIndexes.map{ $0 })\t|\tselExternal: \(self.selection.map{ $0 })
-//              """ )
-    }
-    
-//    public func collectionView(_ collectionView: NSCollectionView, shouldDeselectItemsAt indexPaths: Set<IndexPath>) -> Set<IndexPath> {
-//        return indexPaths
-//    }
-    
     ///////////////////////////////
     // HELPERS Drag
     ///////////////////////////////
+    ///
+    //NSCollectionViewDelegate
     public func collectionView(_ collectionView: NSCollectionView, updateDraggingItemsForDrag draggingInfo: NSDraggingInfo) {
         print("collectionView")
     }
     
+    //NSCollectionViewDelegate
     public func collectionView(_ collectionView: NSCollectionView, draggingImageForItemsAt indexPaths: Set<IndexPath>, with event: NSEvent, offset dragImageOffset: NSPointPointer) -> NSImage {
+        
         NSImage(named: "square.and.arrow.down.on.square.fill")!
     }
     
@@ -121,22 +80,15 @@ where T.Index == Int {
     public func collectionView(_ collectionView: NSCollectionView, pasteboardWriterForItemAt index: Int) -> NSPasteboardWriting? {
         print("check for dragHandler is not nil")
         
-        guard let itemsView = self.itemsView as? FBCollectionView<URL, FileTile>?,
-              let dragHandler = itemsView?.dragHandler,
-              let item = itemsView?.items[index]
+        guard let collectionView = self.collectionView as? FBCollectionView<URL, FileTile>?,
+              let dragHandler = collectionView?.dragHandler,
+              let item = collectionView?.items[index]
         else { return nil }
         
         print("dragHandler is not nil!!!!!")
         
         return dragHandler(item)
     }
-    
-    ///////////////////////////////
-    // HELPERS
-    ///////////////////////////////
-    public func collectionView(_ collectionView: NSCollectionView, numberOfItemsInSection section: Int) -> Int { items.count }
-    
-    required init?(coder: NSCoder) { fatalError("init(coder:) has not been implemented") }
     
     //////////////////////////////
     //QuickLook
@@ -147,27 +99,9 @@ where T.Index == Int {
         let spaceKeyCode: UInt16 = 49
         switch event {
         case _ where event.keyCode == spaceKeyCode:
-            guard isQuickLookEnabled else {
-                return false
-            }
+            guard isQuickLookEnabled else { return false }
             
-            print("Space pressed & QuickLook is enabled.")
-            if let quickLook = QLPreviewPanel.shared() {
-                quickLook.currentPreviewItemIndex = selection.sorted(by: <).first ?? 0
-                
-                print("preview idx: \(quickLook.currentPreviewItemIndex)")
-                
-                let isQuickLookShowing = QLPreviewPanel.sharedPreviewPanelExists() && quickLook.isVisible
-                
-                if (isQuickLookShowing) {
-                    quickLook.reloadData()
-                } else {
-                    quickLook.dataSource = self
-                    quickLook.delegate = self
-                    quickLook.center()
-                    quickLook.makeKeyAndOrderFront(nil)
-                }
-            }
+            enableQuickLookPanel()
             
             return true
         default:
@@ -176,71 +110,138 @@ where T.Index == Int {
     }
     
     // QLPreviewPanelDataSource
-    public func numberOfPreviewItems(in panel: QLPreviewPanel!) -> Int {
-        return isQuickLookEnabled ? items.count : 0
+    public func numberOfPreviewItems(in panel: QLPreviewPanel!) -> Int { isQuickLookEnabled ? items.count : 0 }
+    
+    // QLPreviewPanelDelegate               | Inspired by https://stackoverflow.com/a/33923618/788168
+    public func previewPanel(_ panel: QLPreviewPanel!, handle event: NSEvent!) -> Bool { quickLookKeyboardArrowsController(event: event) }
+    
+    //QLPreviewPanelDataSource
+    public func previewPanel(_ panel: QLPreviewPanel!, previewItemAt index: Int) -> QLPreviewItem! { previewItemAt(index: index) }
+    
+    ///////////////////////////////
+    // HELPERS
+    ///////////////////////////////
+    public func collectionView(_ collectionView: NSCollectionView, numberOfItemsInSection section: Int) -> Int { items.count }
+    
+    required init?(coder: NSCoder) { fatalError("init(coder:) has not been implemented") }
+}
+
+////////////////////////
+///QuickLook
+////////////////////////
+fileprivate extension NSCollectionController {
+    var isQuickLookEnabled: Bool { quickLookHandler() != nil }
+    
+    var isQuickLookShowing: Bool { QLPreviewPanel.sharedPreviewPanelExists() && (QLPreviewPanel.shared()?.isVisible ?? false) }
+    
+    func previewItemAt(index: Int) -> QLPreviewItem? {
+        guard isQuickLookEnabled else { return nil }
+        
+        // If no URLs, return.
+        guard let urls = quickLookHandler() else { return nil }
+        
+        self.selection = [index-1]
+        
+        return urls[safe: index] as QLPreviewItem?
     }
     
-    // QLPreviewPanelDelegate
-    // Inspired by https://stackoverflow.com/a/33923618/788168
-    func previewPanel(_ panel: QLPreviewPanel!, handle event: NSEvent!) -> Bool {
-        if (event.type == .keyDown) {
-            print("Key down: \(event.keyCode); modifiders: \(event.modifierFlags)")
+    func quickLookKeyboardArrowsController(event: NSEvent) -> Bool {
+        guard event.type == .keyDown else { return false }
+        
+        print("Key down: \(event.keyCode); modifiders: \(event.modifierFlags)")
+        
+        switch event.keyCode {
+        case FBKey.upArrow: fallthrough
+        case FBKey.rightArrow: fallthrough
+        case FBKey.downArrow: fallthrough
+        case FBKey.leftArrow:
+            // Don't pass through shift-selection keys
+            guard event.modifierFlags.contains(.shift) == false else { return false }
+            // Don't pass through command-selection keys
+            guard event.modifierFlags.contains(.command) == false else { return false }
             
-            // TODO: forward Option+Backspace to the NSCollectionView?
-            let upArrow: UInt16 = 126
-            let rightArrow: UInt16 = 124
-            let downArrow: UInt16 = 125
-            let leftArrow: UInt16 = 123
-            switch event.keyCode {
-            case upArrow: fallthrough
-            case rightArrow: fallthrough
-            case downArrow: fallthrough
-            case leftArrow:
-                // Don't pass through shift-selection keys.
-                guard event.modifierFlags.contains(.shift) == false else { return false }
-                
-                // Though I believe the event is handled by QL when
-                // multiple items exist, just be safe.
-                if (selection.count <= 1) {
-                    // Forward the keydown event to the NSCollectionView, which will handle moving focus.
-                    
-                    parent?.keyDown(with: event)
-                    return true
-                }
-            default: break
-                // no-op
-            }
+            // Though I believe the event is handled by QL when
+            // multiple items exist, just be safe.
+            //if selection.count <= 1 {
+                // Forward the keydown event to the NSCollectionView, which will handle moving focus.
+                collectionView?.keyDown(with: event)
+                return true
+//            }
+        default:
+            break
         }
         
         return false
     }
     
-    public func previewPanel(_ panel: QLPreviewPanel!, previewItemAt index: Int) -> QLPreviewItem! {
-        guard isQuickLookEnabled else {
-            return nil
-        }
+    func enableQuickLookPanel() {
+        print("Space pressed & QuickLook is enabled.")
         
-        guard let urls = quickLookHandler() else {
-            // If no URLs, return.
-            return nil
-        }
+        guard let quickLook = QLPreviewPanel.shared() else { return }
         
-        return urls[safe: index] as QLPreviewItem?
+        quickLook.currentPreviewItemIndex = selection.sorted(by: <).first ?? 0
+        
+        print("preview idx: \(quickLook.currentPreviewItemIndex)")
+        
+        if isQuickLookShowing {
+            quickLook.reloadData()
+        } else {
+            quickLook.dataSource = self
+            quickLook.delegate = self
+            quickLook.center()
+            quickLook.makeKeyAndOrderFront(nil)
+        }
     }
 }
 
+struct FBKey {
+    static let upArrow: UInt16 = 126
+    static let rightArrow: UInt16 = 124
+    static let downArrow: UInt16 = 125
+    static let leftArrow: UInt16 = 123
+}
+
 fileprivate extension NSCollectionController {
-    var isQuickLookEnabled: Bool {
-        return quickLookHandler() != nil
+    func reloadVisibles() {
+        guard let collectionView = collectionView else { return }
+        collectionView.reloadItems(at: collectionView.indexPathsForVisibleItems())
     }
     
-    func itemIsNil(_ indexPath: IndexPath) -> Bool {
+    func urlIsNilBy(_ indexPath: IndexPath) -> Bool {
         guard let url = items[indexPath.intValue] as? URL? else { return false }
         return url == nil
     }
     
     func exceptNilItems(_ indexPaths: Set<IndexPath>) -> Set<IndexPath> {
         guard let items = self.items as? [URL?] else { return indexPaths }
+        
         return indexPaths.filter{ items[$0.intValue] != nil }
+    }
+    
+    func selectionLog(_ title: String, _ indexPaths: Set<IndexPath>, _ collectionView: NSCollectionView) {
+//        print("""
+//              \(title):\t changes: \(indexPaths.map{ $0.intValue })\t|\tselInternal: \(collectionView.selectionIndexes.map{ $0 })\t|\tselExternal: \(self.selection.map{ $0 })
+//              """ )
+    }
+    
+//    public func collectionView(_ collectionView: NSCollectionView, shouldDeselectItemsAt indexPaths: Set<IndexPath>) -> Set<IndexPath> {
+//        return indexPaths
+//    }
+    
+    func makeItemForCV(byIndexPath indexPath: IndexPath, collectionView: NSCollectionView) -> NSCollectionViewItem {
+        let item = collectionView.makeItem(withIdentifier: NSUserInterfaceItemIdentifier("NSCollectionViewItem"), for: indexPath)
+        
+        if let item = item as? CollectionViewItem {
+            let hosting = NSHostingView(rootView: factory(items[indexPath.item], indexPath))
+            
+            item.container.views.forEach { item.container.removeView($0) }
+            item.container.addView(hosting, in: .center)
+            
+//            if urlIsNilBy(indexPath) {
+//                item.acceptsMouseDown = false
+//            }
+        }
+        
+        return item
     }
 }
