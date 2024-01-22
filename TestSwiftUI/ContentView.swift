5c279ff29c0dd597f83fe0da1c5b4a637c6fa20b
UKS <skulptorrr@gmail.com>
2024-01-23 01:22:19 +0200

    Depression test p1

--- TestSwiftUI/ContentView.swift
+++ TestSwiftUI/ContentView.swift
@@ -1,77 +1,77 @@
-import Combine
-import SwiftUI
-
-@available(macOS 12.0, *)
-struct ContentView: View {
-    @State var text: String = textSample { didSet { print(text) }}
-    
-    var body: some View {
-        ZStack {
-            VStack {
-                Spacer()
-                
-                Text("Text: \(text)")
-                
-                Spacer()
-            }
-            
-            VStack {
-                Spacer()
-                
-                DescriptionTextField(text: $text)
-                    .padding(EdgeInsets(top: 3, leading: 3, bottom: 6, trailing: 3) )
-                    .background(Color.green)
-            }
-        }
-        .frame(minWidth: 450, minHeight: 300)
-        .preferredColorScheme(.light)
-    }
-}
-
-let textSample =
-"""
-hello 1
-hello 2
-"""
-
-
-//hello 3
-//hello 4
-//hello 5
-//hello 6
-//hello 7
-//hello 8
-
-
-
-
-
-
-
-
-
-
-@available(OSX 11.0, *)
-public struct AttributedTextOld: NSViewRepresentable {
-    @Binding var text: NSAttributedString
-    private let selectable: Bool
-    
-    public init(attributedString: Binding<NSAttributedString>, selectable: Bool = true) {
-        _text = attributedString
-        self.selectable = selectable
-    }
-    
-    public func makeNSView(context: Context) -> NSTextField {
-        let textField = NSTextField(labelWithAttributedString: text)
-        textField.preferredMaxLayoutWidth = textField.frame.width
-        textField.allowsEditingTextAttributes = true // Fix of clear of styles on click
-        
-        textField.isSelectable = selectable
-        
-        return textField
-    }
-    
-    public func updateNSView(_ textField: NSTextField, context: Context) {
-        textField.attributedStringValue = $text.wrappedValue
-    }
-}
+//import Combine
+//import SwiftUI
+//
+//@available(macOS 12.0, *)
+//struct ContentView: View {
+//    @State var text: String = textSample { didSet { print(text) }}
+//    
+//    var body: some View {
+//        ZStack {
+//            VStack {
+//                Spacer()
+//                
+//                Text("Text: \(text)")
+//                
+//                Spacer()
+//            }
+//            
+//            VStack {
+//                Spacer()
+//                
+//                DescriptionTextField(text: $text)
+//                    .padding(EdgeInsets(top: 3, leading: 3, bottom: 6, trailing: 3) )
+//                    .background(Color.green)
+//            }
+//        }
+//        .frame(minWidth: 450, minHeight: 300)
+//        .preferredColorScheme(.light)
+//    }
+//}
+//
+//let textSample =
+//"""
+//hello 1
+//hello 2
+//"""
+//
+//
+////hello 3
+////hello 4
+////hello 5
+////hello 6
+////hello 7
+////hello 8
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//@available(OSX 11.0, *)
+//public struct AttributedTextOld: NSViewRepresentable {
+//    @Binding var text: NSAttributedString
+//    private let selectable: Bool
+//    
+//    public init(attributedString: Binding<NSAttributedString>, selectable: Bool = true) {
+//        _text = attributedString
+//        self.selectable = selectable
+//    }
+//    
+//    public func makeNSView(context: Context) -> NSTextField {
+//        let textField = NSTextField(labelWithAttributedString: text)
+//        textField.preferredMaxLayoutWidth = textField.frame.width
+//        textField.allowsEditingTextAttributes = true // Fix of clear of styles on click
+//        
+//        textField.isSelectable = selectable
+//        
+//        return textField
+//    }
+//    
+//    public func updateNSView(_ textField: NSTextField, context: Context) {
+//        textField.attributedStringValue = $text.wrappedValue
+//    }
+//}
--- TestSwiftUI/TestSwiftUIApp.swift
+++ TestSwiftUI/TestSwiftUIApp.swift
@@ -1,186 +1,69 @@
 import SwiftUI
 import AVKit
 import Witness
+import MoreSwiftUI
 
 @main
 struct TestSwiftUIApp: App {
     var body: some Scene {
         WindowGroup {
-            MainView()
-//            ContentView()
-//            DirContentView()
+            ContentView()
         }
     }
 }
 
-//struct DirContentView: View {
-//    @State var files: [String] = try! FileManager.default.contentsOfDirectory(atPath: "/Users/uks/Dox/pix")
-//    @State var sel: Set<String> = []
-//
-//    var body: some View {
-//        DirectoryTableView(dirContent: $files, selection: $sel)
-//    }
-//}
-
-struct MainView: View {
-    @ObservedObject var model = MainViewModel()
-    
+struct ContentView: View {
     var body: some View {
-        Text("hello")
-            .padding()
-    }
-}
-
-
-class MainViewModel: ObservableObject {
-    let witness: Witness
-    
-    init() {
-        let desktopPath = "/"
-        
-        self.witness = Witness(paths: [desktopPath], flags: [.NoDefer, .FileEvents], latency: 0) { events in
-            var events = events
-                .filter({ !$0.path.ends(with: ".DS_Store") })
-                .filter{ !"\($0)".ends(with: "flags: Item Is File)") } // empty event with no usefull data
-                .filter{ $0.flags.contains(oneOf: [.ItemModified, .ItemCreated, .ItemRemoved, .ItemRenamed]) }
-            
-            guard events.count > 0 else { return }
-            print("\n\n\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
-            
-            var uksEvents = [UKSFileEvent]()
-            
-            // REMOVED to Bin
-            if events.allSatisfy({ $0.flags.contains(.ItemRenamed) && !FileManager.default.fileExists(atPath: $0.path) })
-            {
-                events.forEach { uksEvents.append(.removed(from: $0.path) ) }
-                events = []
-            }
-            else
-            //RENAMED
-            if (events.count % 2 == 0) && events.allSatisfy({ $0.flags.contains(.ItemRenamed) }) {
-                stride(from: 0, to: events.count - 1, by: 2).forEach { idx in
-                    uksEvents.append(.renamed(from: events[idx].path, to: events[idx+1].path))
-                }
-                events = []
-            }
-            
-            // REMOVED with skipping RecicleBin
-            events.filter({ $0.flags.contains(.ItemRemoved) }).forEach { event in
-                events.forEach { uksEvents.append(.removed(from: $0.path) ) }
-                events = []
-                events.removeFirst{ $0 == event }
-            }
-            
-            events.filter({ $0.flags.contains(.ItemCreated) }).forEach { event in
-                let currPath = event.path
-                uksEvents.append( .modified(currPath) )
-                events.removeFirst{ $0 == event }
-            }
-            
-            events.filter({ $0.flags.contains(.ItemModified) }).forEach { event in
-                let currPath = event.path
-                uksEvents.append( .modified(currPath) )
-                events.removeFirst{ $0 == event }
-            }
-            
-            events.filter({ $0.flags.contains(.ItemRenamed) }).forEach { event in
-                if FileManager.default.fileExists(atPath: event.path) {
-                    uksEvents.append(.modified(event.path))
-                    events.removeFirst{ $0 == event }
-                } else {
-                    uksEvents.append(.removed(from: event.path))
-                    events.removeFirst{ $0 == event }
-                }
-            }
-            
-            print("\n----\nuksEvents: \n\(uksEvents)")
-            
-            if events.count > 0 {
-                print("MISSED Events: \(events)\n------------\n\n\n")
+        VStack {
+            ScrollView {
                 
-                fatalError("horrible error")
+                VStack(alignment: .leading, spacing: 30) {
+                    ForEach(AaronBack.allCases, id: \.self) { item in
+                        VStack(alignment: .leading, spacing: 5) {
+                            ForEach(item.asAnswerKeys(), id: \.self) { answerKey in
+                                HStack {
+                                    Text("lastWeekI".localized)
+                                    
+                                    Text( answerKey.localized )
+                                }
+                            }
+                            
+                            Divider()
+                        }
+                    }
+                }
+                .padding(30)
             }
         }
     }
 }
 
-
-enum UKSFileEvent {
-    case renamed(from: String, to: String)
-    case modified(String)
-    case removed(from: String)
+enum AaronBack: String, RawRepresentable, CaseIterable {
+    case q01
+    case q02
+    case q03
+    case q04
+    case q05
+    case q06
+    case q07
+    case q08
+    case q09
+    case q10
+    case q11
+    case q12
+    case q13
+    case q14
+    case q15
+    case q16
+    case q17
+    case q18
+    case q19
+    case q20
+    case q21
 }
 
-extension FileEvent {
-    var uksIsDeleted: Bool {
-        flags.contains(.ItemRenamed) && !FileManager.default.fileExists(atPath: self.path)
-    }
-}
-
-extension FileEvent: Equatable {
-    public static func == (lhs: FileEvent, rhs: FileEvent) -> Bool {
-        lhs.path == rhs.path &&
-        lhs.flags == rhs.flags
-    }
-    
-    
-}
-
-/*
-* Видалений в корзину:
-    * FileEvent(path: "/Users/uks/Desktop/Untitled_.rtf", flags: Item Renamed, Item Is File)
-    * тепер файлу не існує
- 
-* Відновлений з корзини:
-    * FileEvent(path: "/Users/uks/Desktop/Untitled_.rtf", flags: Item Renamed, Item Is File)
-    * тепер файл існує
- 
- * перейменовується папка
- * Перейменувати файл:
-     * FileEvent(path: "/Users/uks/Desktop/Untitled_.rtf", flags: Item Renamed,Item Is File)
-     * FileEvent(path: "/Users/uks/Desktop/Untitled.rtf" , flags: Item Renamed,Item Is File)
- 
- * Переписати файл поверх перенісши його:
-    * FileEvent(path: "/Users/uks/Desktop/Untitled.rtf", flags: Item Removed,Item Is File)
-    * FileEvent(path: "/Users/uks/Desktop/untitled folder_/Untitled.rtf", flags: Item Renamed,Item Is File)
-    * FileEvent(path: "/Users/uks/Desktop/Untitled.rtf", flags: Item Renamed,Item Is File)
- 
- * скопіювати папку з файлами
-    * створюється папка
-    * створюються всі підфайли купою івент сетів
-
-
- 
- 
- 
- * Переписати файл поверх скопіювавши його:
-    * FileEvent(path: "/Users/uks/Desktop/Untitled.rtf", flags: Item Removed,Item Is File)
-    * FileEvent(path: "/Users/uks/Desktop/untitled folder_/Untitled.rtf", flags: Item Inode Meta Modification,Item Renamed,Item Xattr Modification,Item Is File)
-    * FileEvent(path: "/Users/uks/Desktop/Untitled.rtf", flags: Item Is File)
-    * FileEvent(path: "/Users/uks/Desktop/Untitled.rtf", flags: Item created,Item Inode Meta Modification,Item Change Owner,Item Is File)
-    
-* Створити файл:
-    * FileEvent(path: "/Users/uks/Desktop/Untitled.rtf", flags: Item Renamed,Item Xattr Modification,Item Is File)
-    * FileEvent(path: "/Users/uks/Desktop/Untitled.rtf", flags: Item Xattr Modification,Item Is File)
-    
-* Переписати 2 файли:
-    * FileEvent(path: "/Users/uks/Desktop/untitled folder_/book 2 copy 10.txt", flags: Item Renamed,Item Is File)
-    * FileEvent(path: "/Users/uks/Desktop/book 2 copy 10.txt", flags: Item Renamed,Item Is File)
-    * FileEvent(path: "/Users/uks/Desktop/untitled folder_/book 2 copy 11.txt", flags: Item Renamed,Item Is File)
-    * FileEvent(path: "/Users/uks/Desktop/book 2 copy 11.txt", flags: Item Renamed,Item Is File)
- 
-*/
-
-
-////////////////////
-// HELPERS
-///////////////////
-fileprivate extension FileEventFlags {
-    func contains(oneOf members: [FileEventFlags]) -> Bool {
-        for member in members {
-            if self.contains(member) { return true }
-        }
-        
-        return false
+extension AaronBack {
+    func asAnswerKeys() -> [String] {
+        return Array(1...4).map { self.rawValue + ".answer"+"\($0)" }
     }
 }
