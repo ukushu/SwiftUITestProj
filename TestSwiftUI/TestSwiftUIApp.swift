1f518aa479f30f6c4c8b5c8f51cf2809730c768c
UKS <skulptorrr@gmail.com>
2024-01-23 16:56:51 +0200

    Depression 2

--- TestSwiftUI/TestSwiftUIApp.swift
+++ TestSwiftUI/TestSwiftUIApp.swift
@@ -2,6 +2,7 @@ import SwiftUI
 import AVKit
 import Witness
 import MoreSwiftUI
+import Essentials
 
 @main
 struct TestSwiftUIApp: App {
@@ -13,53 +14,148 @@ struct TestSwiftUIApp: App {
 }
 
 struct ContentView: View {
+    @ObservedObject var model = ContentViewModel()
+    
     var body: some View {
         VStack {
+            if model.displayResults {
+                ResultsView()
+            } else {
+                TestView()
+            }
+        }
+    }
+    
+    @ViewBuilder
+    func TestView() -> some View {
+        VStack {
             ScrollView {
-                
-                VStack(alignment: .leading, spacing: 30) {
-                    ForEach(AaronBack.allCases, id: \.self) { item in
-                        VStack(alignment: .leading, spacing: 5) {
-                            ForEach(item.asAnswerKeys(), id: \.self) { answerKey in
-                                HStack {
-                                    Text("lastWeekI".localized)
-                                    
-                                    Text( answerKey.localized )
-                                }
-                            }
+                VStack(alignment: .leading, spacing: 40) {
+                    ForEach(model.testResults.indices, id: \.self) { idx in
+                        HStack {
+                            Text("lastWeekI".localized)
                             
-                            Divider()
+                            VStack{
+                                Picker(selection: $model.testResults[idx].selectedAnswer, label: Text("")) {
+                                    ForEach(model.testResults[idx].answers, id: \.self) { answerKey in
+                                        Text( answerKey.localized ).tag(answerKey)
+                                    }
+                                }.pickerStyle(RadioGroupPickerStyle())
+                            }
                         }
                     }
                 }
                 .padding(30)
             }
+            Button("Show results") { model.switchDisplayResults() }
         }
+    }
+    
+    
+    @ViewBuilder
+    func ResultsView() -> some View {
+        VStack {
+            Text("Depression Rate")
+            
+            ZStack {
+                ProgressLine(progressRatio: $model.depressionRate , fillColor: .red)
+                    .frame(height: 30)
+                
+                Text("\( Int(model.points) )/\(model.depressionMax)")
+            }
+            
+            HStack {
+                VStack {
+                    Text("Психічні проявлення")
+                    
+                    ZStack {
+                        ProgressLine(progressRatio: $model.psihicRate , fillColor: .red)
+                            .frame(height: 20)
+                        
+                        Text("\( Int(model.points) )/\(model.psihicRateMax)")
+                    }
+                }
+                
+                VStack {
+                    Text("Фізичні проявлення")
+                    
+                    ZStack {
+                        ProgressLine(progressRatio: $model.phisicRate , fillColor: .red)
+                            .frame(height: 20)
+                        
+                        Text("\(Int(model.points))/\(model.phisicRateMax)")
+                    }
+                }
+            }
+            
+            HStack {
+                Button("Back") { model.switchDisplayResults() }
+                
+                Button("Ok") { }
+            }
+        }
+        .padding(10)
+    }
+}
+
+class ContentViewModel: ObservableObject {
+    @Published var testResults: [AaronBackPair] = AaronBack.allCases.map{ AaronBackPair(item: $0) }
+    @Published private(set) var displayResults: Bool = true
+    
+    @Published private(set) var points: CGFloat = 0
+    @Published var depressionRate: CGFloat = 0
+    let depressionMax: Int = 57
+    
+    @Published var psihicRate: CGFloat = 0
+    let psihicRateMax: Int = 30
+    
+    @Published var phisicRate: CGFloat = 0
+    let phisicRateMax: Int = 30
+    
+    func switchDisplayResults() {
+        if displayResults {
+            displayResults = false
+            return
+        }
+        
+        let groupPoints = testResults.map { item in item.answers.firstIndexInt(where: { $0 == item.selectedAnswer})! }
+        points = CGFloat( groupPoints.reduce(0, +) )
+        
+        depressionRate = points/57
+        
+        var tempPoints = testResults.first(10).map { item in item.answers.firstIndexInt(where: { $0 == item.selectedAnswer})! }
+        psihicRate = CGFloat( tempPoints.reduce(0, +) ) / CGFloat(phisicRateMax)
+        
+        tempPoints = testResults.last(10).map { item in item.answers.firstIndexInt(where: { $0 == item.selectedAnswer})! }
+        phisicRate = CGFloat( tempPoints.reduce(0, +) ) / CGFloat(phisicRateMax)
+        
+        displayResults.toggle()
+    }
+    
+    /*
+     Общий балл по шкале может интерпретироваться следующим образом:
+     • 0−9 – отсутствие депрессивных симптомов;
+     • 10−18 – субдепрессия, умеренная депрессия;
+     • 19−29 – выраженная депрессия средней тяжести;
+     • 30−63 – тяжелая депрессия.
+     Результат онлайн-теста сам по себе не может быть критерием для постановки диагноза депрессии. Диагноз может поставить только специалист по совокупности факторов.
+     */
+}
+
+struct AaronBackPair: Hashable {
+    let item: AaronBack
+    var selectedAnswer: String
+    var answers: [String]
+    
+    init(item: AaronBack) {
+        self.item = item
+        answers = item.asAnswerKeys()
+        selectedAnswer = answers.first!
     }
 }
 
 enum AaronBack: String, RawRepresentable, CaseIterable {
-    case q01
-    case q02
-    case q03
-    case q04
-    case q05
-    case q06
-    case q07
-    case q08
-    case q09
-    case q10
-    case q11
-    case q12
-    case q13
-    case q14
-    case q15
-    case q16
-    case q17
-    case q18
-    case q19
-    case q20
-    case q21
+    case q01,q02,q03,q04,q05,q06,q07,q08,q09,q10,q11,q12,q13,q14,q15,q16,q17,q18,q19,q20,q21
 }
 
 extension AaronBack {
